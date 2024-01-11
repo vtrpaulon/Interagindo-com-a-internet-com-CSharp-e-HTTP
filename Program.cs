c8ff69f7a55de468543beeeb3fad3690ac61f1af
VitorPaulon <71847592+vtrpaulon@users.noreply.github.com>
2024-01-11 17:24:21 +0000

    commit 11-01-24

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
 using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-    Console.WriteLine("Qual é o seu nome?");
+class Program
+{
+    public static async Task Main(string[] args)
+    {   
+        using (HttpClient client = new HttpClient())
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos/";
+            
+            HttpResponseMessage response = await client.GetAsync(url);
 
-    Console.ReadLine();
-  }
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(data);
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+            }
+        }
+
+        Console.ReadLine();
+    }
 }
