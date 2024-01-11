04ed1cec00b6f7c6934a7f94396de7da94b40453
VitorPaulon <71847592+vtrpaulon@users.noreply.github.com>
2024-01-11 16:02:04 +0000

    commit

--- Program.cs
+++ Program.cs
@@ -3,5 +3,8 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
+    Console.WriteLine("Qual é o seu nome?");
+
+    Console.ReadLine();
   }
 }
